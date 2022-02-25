b0fba4ef8ce3060336b7a1df4e3b386f874ca955
whosane <huseinzb@gmail.com>
2022-02-25 14:26:20 -0600

    Update plantuml.js

--- src/plantuml.js
+++ src/plantuml.js
@@ -1,67 +1,66 @@
-import skin from './skin'
-import { encode } from 'plantuml-encoder'
+import skin from './skin';
+import {
+	encode
+}
+from 'plantuml-encoder';
 
-var LANG = 'plantuml'
-var SELECTOR = 'pre[data-lang="' + LANG + '"]'
+var LANG = 'plantuml';
+var SELECTOR = 'pre[data-lang="' + LANG + '"]';
 
 export function plant(content, config) {
-  var content = skin(config.skin) + content
-  var urlPrefix = (config.serverPath || '//www.plantuml.com/plantuml/svg/')
-  if (config.renderSvgAsObject) {
-    var svgUrl = urlPrefix + encode(createUrls(content))
-    return '<object type="image/svg+xml" data="' + svgUrl + '" />'
-  }
-  var svgUrl = urlPrefix + encode(createUrls(content))
-  return '<object type="image/svg+xml" data="' + svgUrl + '" />'
+	content = skin(config.skin) + content;
+	var urlPrefix = (config.serverPath || '//www.plantuml.com/plantuml/svg/');
+	var svgUrl = urlPrefix + encode(createUrls(content));
+	return '<object type="image/svg+xml" data="' + svgUrl + '" />';
 }
 
 function createUrls(content) {
-  var location = window.location.toString();
-  var currenturl = location.substring(0, location.lastIndexOf('/') + 1)
+	var location = window.location.toString();
+	var currenturl = location.substring(0, location.lastIndexOf('/') + 1);
 
-  return content.replace(/\[\[\$((?:\.?\.\/)*)/g, resolvePath)
+	return content.replace(/\[\[\$((?:\.?\.\/)*)/g, resolvePath);
 
-  // solution taken from docsify codebase
-  function resolvePath(_, path) {
-    var segments = (currenturl + path).split('/')
-    var resolved = []
-    for (var i = 0, len = segments.length; i < len; i++) {
-      var segment = segments[i]
-      if (segment === '..') {
-        resolved.pop()
-      } else if (segment !== '.') {
-        resolved.push(segment)
-      }
-    }
-    return '[[' + resolved.join('/')
-  }
+	// solution taken from docsify codebase
+	function resolvePath(_, path) {
+		var segments = (currenturl + path).split('/');
+		var resolved = [];
+		for (var i = 0, len = segments.length; i < len; i++) {
+			var segment = segments[i];
+			if (segment === '..') {
+				resolved.pop();
+			} else if (segment !== '.') {
+				resolved.push(segment);
+			}
+		}
+		return '[[' + resolved.join('/');
+	}
 }
 
 export function replace(content, selector, config) {
-  var dom = window.Docsify.dom
-  var $ = dom.create('span', content)
+	var dom = window.Docsify.dom;
+	var $ = dom.create('span', content);
 
-  if (!$.querySelectorAll) {
-    return content
-  }
+	if (!$.querySelectorAll) {
+		return content;
+	}
 
-  (dom.findAll($, selector) || []).forEach(function (element) {
-    var parent = element.parentNode
-    var planted = dom.create('div', plant(element.innerText, config))
-    if (parent) {
-      planted.dataset.lang = LANG
-      element.parentNode.replaceChild(planted, element)
-    }
-  })
-  return $.innerHTML
+	(dom.findAll($, selector) || []).forEach(function(element) {
+		var parent = element.parentNode;
+		var planted = dom.create('div', plant(element.innerText, config));
+		if (parent) {
+			planted.dataset.lang = LANG;
+			element.parentNode.replaceChild(planted, element);
+		}
+	});
+	return $.innerHTML;
 }
 
 export function install(hook, vm) {
-  const config = Object.assign({}, {
-    skin: 'default',
-    renderSvgAsObject: true
-  }, vm.config.plantuml)
-  hook.afterEach(function (content) {
-    return replace(content, SELECTOR, config)
-  })
+	const config = Object.assign({}, {
+		skin: 'default',
+		renderSvgAsObject: true
+	}, vm.config.plantuml);
+	hook.afterEach(function(content) {
+		return replace(content, SELECTOR, config);
+	});
 }
